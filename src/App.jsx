e87cc78d6dc71378fc950a583b8799241da053a1
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2024-12-26 15:15:44 -0500

    refresh

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,11 @@
-import './styles/App.css';
-import routesConfig from './routes/routesConfig';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import { createContext, useState } from 'react';
-
-const router = createBrowserRouter(routesConfig);
-
-// export const ExpenseContext = createContext({
-//   expenseTable: [],
-//   addToTable: () => {},
-// });
+import Register from './Register';
 
 function App() {
+
   return (
-    // <ExpenseContext.Provider value={{ expenseTable, addToTable }}>
-    <RouterProvider router={router} />
-    // </ExpenseContext.Provider>
+    <main className="App">
+      <Register />
+    </main>
   );
 }
 
--- src/routes/Root/StyledRoot.js
+++
@@ -1,4 +0,0 @@
-import styled from 'styled-components';
-const StyledRoot = styled.main``;
-
-export default StyledRoot;
