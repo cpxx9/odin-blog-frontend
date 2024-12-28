266e3d5123faf29f80f1117308be1eb1c90098a1
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2024-12-28 13:22:14 -0500

    feat: basic log in form

--- src/routes/Login/Login.jsx
+++ src/routes/Login/Login.jsx
@@ -1,10 +1,79 @@
+import { useEffect, useRef, useState } from 'react';
+import { Link } from 'react-router-dom';
 import StyledLogin from './StyledLogin';
 
 const Login = () => {
+  const userRef = useRef();
+  const errRef = useRef();
+
+  const [user, setUser] = useState('');
+  const [pwd, setPwd] = useState('');
+  const [errMsg, setErrMsg] = useState('');
+  const [success, setSuccess] = useState('');
+
+  useEffect(() => {
+    userRef.current.focus();
+  }, []);
+
+  useEffect(() => {
+    setErrMsg('');
+  }, [user, pwd]);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setPwd('');
+    setSuccess(true);
+  };
+
   return (
-    <StyledLogin>
-      <h1>Login</h1>
-    </StyledLogin>
+    <>
+      {success ? (
+        <section>
+          <h1>You are logged in!</h1>
+          <br />
+          <p>
+            <Link to="/">Go to home</Link>
+          </p>
+        </section>
+      ) : (
+        <StyledLogin>
+          <p ref={errRef} className={errMsg ? 'errmsg' : 'offscreen'} aria-live="assertive">
+            {errMsg}
+          </p>
+          <h1>Login</h1>
+          <form onSubmit={handleSubmit}>
+            <label htmlFor="username">Username:</label>
+            <input
+              type="text"
+              name="username"
+              id="username"
+              ref={userRef}
+              autoComplete="off"
+              onChange={(e) => setUser(e.target.value)}
+              value={user}
+              required
+            />
+            <label htmlFor="password">Password:</label>
+            <input
+              type="password"
+              name="password"
+              id="password"
+              onChange={(e) => setPwd(e.target.value)}
+              value={pwd}
+              required
+            />
+            <button>Sign in</button>
+          </form>
+          <p>
+            Need an account?
+            <br />
+            <span className="line">
+              <Link to="/register">Sign up</Link>
+            </span>
+          </p>
+        </StyledLogin>
+      )}
+    </>
   );
 };
 
